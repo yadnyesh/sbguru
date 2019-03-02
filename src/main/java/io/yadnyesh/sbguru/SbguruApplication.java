b2583299f83491dbef44abebe8219ce354ba7df2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-02 18:49:30 +0530

    First Commit

---
+++ src/main/java/io/yadnyesh/sbguru/SbguruApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.sbguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbguruApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SbguruApplication.class, args);
+	}
+	
+}
