e7ffc41da7afe6dab3cc54dad000256653a10884
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-02 19:02:36 +0530

    feat: Created Controller for returning Joke

---
+++ src/test/java/io/yadnyesh/sbguru/controller/JokeController.java
@@ -0,0 +1,22 @@
+package io.yadnyesh.sbguru.controller;
+
+import io.yadnyesh.sbguru.service.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+	
+	private JokeService jokeService;
+	
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "chucknorris";
+	}
+}
