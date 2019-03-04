31cb49e3b8b477224500b9bb15d5ec2d1ef621e8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-04 14:53:45 +0530

    feat: Spring Bean Qualifier - PRIMARY Section 3: Lecture 40

--- src/main/java/io/yadnyesh/sbguru/controller/MyController.java
+++ src/main/java/io/yadnyesh/sbguru/controller/MyController.java
@@ -1,10 +1,17 @@
 package io.yadnyesh.sbguru.controller;
 
+import io.yadnyesh.sbguru.service.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-
+	
+	private GreetingService greetingService;
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
 	public void printMessageOnCommandLine() {
 		System.out.println("Hello World");
 	}
